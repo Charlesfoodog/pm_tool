5ca6e679cc1b525bb946a2c29a5c61af7d65bec6
Charles Sublette <charlessublette@Charless-MacBook-Pro.local>
2014-03-24 14:55:58 -0700

    pm tool added index and routes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PmTool::Application.config.secret_key_base = 'b43dd76fce357b5724e5682633f0ea55f6f369e71b06d61b37e232b97fb6c8b709c069a25c23f1684707090f435802702a6af72e4f11d78ff206bef6fafd82cc'
